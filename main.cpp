4c449c2ac388ae130db69cbd9c57229f5a685bd0
FaizanVirani <104014327+FaizanVirani@users.noreply.github.com>
2022-05-02 20:03:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Not all of these need to be included, it's just the list I always go with for smaller projects. When space preservation is required, I'll only use the necessary ones.
 #include <math.h>
 #include <iostream>
 #include <algorithm>
@@ -8,45 +9,68 @@
 #include <cstring>
 using namespace std;
 
-bool validInt(string);
+bool validInt(string); //Data validation
+int gcf(vector<int>); //GCF Finder
 
 int main() {
   bool runAgain = false;
-  
+
+  //Do while to run again
   do {
-  vector<int> multiples;
+  vector<int> factors;
   string answerHolder;
-  string dummy;
 
   int number;
   int num1 = 0;
   int num2 = 0;
   bool found = false;
 
-  cout << "\nEnter your number: ";
-  cin >> number;
-  for(num2 = 0; num2 < number; num2++) {
-    for(num1 = 0; num1 < number; num1++) {
-      if(num1*num2 == number) {
-        for(int i = 0; i < multiples.size(); i++) {
-          if (multiples[i] == num1) {
+  do {
+    cout << "Enter your number: ";
+    getline(cin, answerHolder);
+    if (answerHolder.length() == 0) {
+            answerHolder = "a";
+    }
+  } while(!validInt(answerHolder));
+    number = stoi(answerHolder);
+
+  for(num2 = 0; num2 < number; num2++) { //Checks every first number
+    for(num1 = 0; num1 < number; num1++) { //Checks every second number
+      if(num1*num2 == number) { //Condition to find factors
+        for(int i = 0; i < factors.size(); i++) {
+          if (factors[i] == num1) {
             found = true;
           }
         }
         if(!found) {
-          multiples.push_back(num1);
-          multiples.push_back(num2);
+          factors.push_back(num1);
+          factors.push_back(num2);
         }
       }
     }
   }
 
-    for(int i = 0; i < multiples.size(); i+=2) {
-      cout << endl << multiples[i] << " and " << multiples[i + 1];
+    if(factors.size() == 0) {
+    factors.push_back(number);
+    factors.push_back(1);
+  }
+    
+    for(int i = 0; i < factors.size(); i+=2) {
+      cout << endl << factors[i] << " and " << factors[i + 1]; //Prints factor pairs
     }
-    getline(cin, dummy);
+
+    for(int i = 0; i < factors.size(); i++) {
+      if(factors[i] == 1) {
+        cout << "\n\x1b[31mThis number is prime!\n\x1b[37m"; //Prime
+        break;
+      }
+      if (i == factors.size() - 1) {
+        cout << "\nGreatest Factor: " << *max_element(factors.begin(), factors.end()) << endl; //Finds and prints greatest factor
+      }
+    }
+    gcf(factors);
     do {
-    cout << "\n\nWould you like to view another item? \n1) Yes\n2) No\n";
+    cout << "\nWould you like to find the factors of another number? \n1) Yes\n2) No\n";
     getline(cin, answerHolder);
     if (answerHolder.length() == 0) {
             answerHolder = "a";
@@ -66,7 +90,7 @@ int main() {
   } while(runAgain);
 }
 
-bool validInt(string a) {
+bool validInt(string a) { //Error trapping
   for (int i = 0; i < a.length(); i++) {
     if (!isdigit(a[i])) {
       return false;
@@ -74,4 +98,75 @@ bool validInt(string a) {
     }
   }
   return true;
+}
+
+int gcf(vector<int> fact) {
+  string answerHolder;
+  bool found = false;
+  int number, num1, num2;
+  vector<int> sameNumbers;
+  vector<int> factors;
+
+  do {
+    cout << "\nWould you like to find the GCF of " << fact[0]*fact[1] << " and another number?\n1) Yes\n2) No\n";
+    getline(cin, answerHolder);
+    if (answerHolder.length() == 0) {
+            answerHolder = "a";
+    }
+    if (validInt(answerHolder)) {
+      if (stoi(answerHolder) > 2 || stoi(answerHolder) < 1) {
+      cout << "\nInvalid entry.";
+      answerHolder = "a";
+      }
+    }
+  } while(!validInt(answerHolder));
+    if (answerHolder == "2") {
+      return 0;
+    }
+  
+  do {
+    cout << "Enter your number: ";
+    getline(cin, answerHolder);
+    if (answerHolder.length() == 0) {
+            answerHolder = "a";
+    }
+  } while(!validInt(answerHolder));
+    number = stoi(answerHolder);
+    
+  for(num2 = 0; num2 < number; num2++) { //Checks every first number
+    for(num1 = 0; num1 < number; num1++) { //Checks every second number
+      if(num1*num2 == number) { //Condition to find factors
+        for(int i = 0; i < factors.size(); i++) {
+          if (factors[i] == num1) {
+            found = true;
+            break;
+          }
+        }
+        if(!found) {
+          factors.push_back(num1);
+          factors.push_back(num2);
+        }
+      }
+    }
+  }
+
+  if(factors.size() == 0) {
+    factors.push_back(number);
+    factors.push_back(1);
+  }
+  
+  for(int i = 0; i < factors.size(); i++) {
+    for(int j = 0; j < factors.size(); j++) {
+      if(factors[j] == fact[i]) {
+        sameNumbers.push_back(fact[i]);
+      }
+    }
+  }
+
+  if(sameNumbers.size() == 0) {
+    cout << endl << "\x1b[31mThese numbers have no GCF!\n\x1b[37m" << "------------------------------------";
+  } else {
+    cout << endl << "The GCF of " << fact[0]*fact[1] << " and " << factors[0]*factors[1] << " is " << *max_element(sameNumbers.begin(), sameNumbers.end()) << "\n------------------------------------";
+  }
+  return 0;
 }
